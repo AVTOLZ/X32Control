19960c407ae7a21a65d6058358226e76100c39a6
Tiebe Groosman <tiebe.groosman@gmail.com>
2023-08-22 20:13:05 +0200

    Changes by koend

--- cli/src/main/java/dev/tiebe/avt/x32/commands/FaderPreset.kt
+++ cli/src/main/java/dev/tiebe/avt/x32/commands/FaderPreset.kt
@@ -5,13 +5,23 @@ import dev.tiebe.avt.x32.api.getChannel
 
 class FaderPreset(private val osc: OSCController): Command {
     override var arguments: List<Any> = listOf()
-    private val off = List(16) { 0.0 }
+    private val off = List(16) { 0.0f }
+    private val line = List(16) { it / 15f }
+    private val zigzag = List(16) { it % 2f }
+    private val line8 = List(16) { (it % 8f) / 7}
+    private val 
     override fun setArguments(args: List<String>): Command? {
         if(args.size != 2) {
             return null
         } else {
             when(args[1]) {
-                "off"   -> arguments = off
+                "off"       -> arguments = off
+                "line"      -> arguments = line
+                "zigzag"    -> arguments = zigzag
+                "line8"     -> arguments = line8
+                "sine"      -> arguments = sine
+
+                else        -> print("Unknown command")
             }
         }
         return this
